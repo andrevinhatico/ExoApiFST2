9b618e2d58cbdbbfcbd2b5b69f179522f48c7502
Andre Ribeiro <andrebahia7@gmail.com>
2022-08-13 21:50:20 -0300

    Inicio projeto

---
+++ dbExoAPI.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE ExoApi
+GO
+
+USE ExoApi
+GO
+
+CREATE TABLE Projetos (
+    Id INT PRIMARY KEY IDENTITY,
+    Titulo VARCHAR(150) NOT NULL,
+    StatusProj BIT NOT NULL,
+    DataInicio DATE,
+	Tecnologias VARCHAR(150),
+	Requisitos VARCHAR(150),
+	Area VARCHAR(150)
+)
+GO
+
+INSERT INTO Projetos (Titulo, StatusProj, DataInicio,Tecnologias, Requisitos, Area) 
+VALUES ('Titulo A', 1, 01/06/2022, 'Tecnologia X', 'Requisito Y', 'Area Z')
+GO
+
+SELECT FROM Projetos
+
+
+CREATE TABLE Usuarios (
+    Id INT PRIMARY KEY IDENTITY,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Senha VARCHAR(120) NOT NULL,
+	Tipo CHAR(1) NOT NULL
+)
+GO
+
+INSERT INTO Usuarios VALUES ('email@sp.br', '1234', 1)
+GO
+
+SELECT * FROM Usuarios WHERE email = 'email@sp.br' AND senha = '1234'
+GO
+
+
+SELECT FROM Usuarios
